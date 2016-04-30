d7dd60a74e6a07f31415d2533361dd938bbf88fe
Pablo Guillama <paguillama@gmail.com>
2016-04-30 00:53:07 -0300

    Adds console log lines

--- index-spec.js
+++ index-spec.js
@@ -2,7 +2,10 @@
 
 describe('travis test specs', function () {
 
+  console.log('it should be executed - 1');
+
   it('should pass', function () {
+    console.log('it should be executed - 2');
     expect(1).toBeDefined();
   });
 
